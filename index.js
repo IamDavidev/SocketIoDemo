6158a05ae783bf3c7b4571dad973bc8a50cf715a
itaDavidev <davidlez2mgk@gmail.com>
2022-01-27 20:40:53 -0600

    🌐 📝 new chanel socket io

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('chat message',msg =>{
+        console.log('message: ' + msg);
+    })
   });
 
 
--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
-const sockte = io();
+const socket = io();
 console.log('socket is running');
+
+
+const form = document.querySelector('#formChat');
+const messageInput = document.querySelector('#inputChat');
+console.log(form);
+console.log(messageInput);
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    console.log('submit', socket.emit());
+    socket.emit('chat message', messageInput.value);
+    messageInput.value = '';
+});
